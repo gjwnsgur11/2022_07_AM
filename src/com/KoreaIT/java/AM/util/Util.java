1978df95161b27aa2ffe30b7328803e7db55634c
gjwnsgur11 <gjwnsgur1771@gmail.com>
2022-08-12 15:40:01 +0900

    게시판 만들기 정리하기까지

--- src/com/KoreaIT/java/AM/util/Util.java
+++ src/com/KoreaIT/java/AM/util/Util.java
@@ -1,10 +1,10 @@
-package com.KoreaIT.java.AM;
+package com.KoreaIT.java.AM.util;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Util {
-	//현재 날짜 시간
+	// 현재 날짜 시간
 	public static String getDateStr() {
 		SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
