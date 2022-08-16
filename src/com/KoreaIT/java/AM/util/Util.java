dcb7d3c0f2f297a5d8197859b00ff454e86c5559
gjwnsgur11 <gjwnsgur1771@gmail.com>
2022-08-16 15:37:41 +0900

    ArticleController 도입

--- src/com/KoreaIT/java/AM/util/Util.java
+++ src/com/KoreaIT/java/AM/util/Util.java
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Util {
-	//현재 날짜 시간
+	// 현재 날짜 시간
 	public static String getDateStr() {
 		SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
